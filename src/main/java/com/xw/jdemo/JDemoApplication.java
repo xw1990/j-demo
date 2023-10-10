fb6ea2a45947f6bebb742d0893d249e5e1d7a746
厉风 <liuwuchao@xiaohongshu.com>
2023-10-10 16:45:23 +0800

    init

---
+++ src/main/java/com/xw/jdemo/JDemoApplication.java
@@ -0,0 +1,13 @@
+package com.xw.jdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xw/jdemo/JDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xw.jdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
